90c540d22afaf33717d446106f5eaab1a66a8dc0
Mohamed Abukar <mo-abukar10@hotmail.com>
2025-08-09 16:51:30 +0100

    locals

--- modules/vpc/flowlogs.tf
+++ modules/vpc/flowlogs.tf
@@ -1,17 +1,20 @@
 resource "aws_cloudwatch_log_group" "flow" {
+  count             = var.enable_flow_logs ? 1 : 0
   name              = "/aws/vpc/${aws_vpc.this.id}/flowlogs"
   retention_in_days = 90
-  kms_key_id        = aws_kms_key.flow.arn
+  kms_key_id        = aws_kms_key.flow[0].arn
   tags              = var.tags
 }
 
 resource "aws_kms_key" "flow" {
+  count               = var.enable_flow_logs ? 1 : 0
   description         = "KMS for VPC flow logs"
   enable_key_rotation = true
   tags                = var.tags
 }
 
 resource "aws_iam_role" "flow" {
+  count              = var.enable_flow_logs ? 1 : 0
   name               = "${var.name}-flowlogs"
   assume_role_policy = data.aws_iam_policy_document.flow_assume.json
   tags               = var.tags
@@ -28,8 +31,9 @@ data "aws_iam_policy_document" "flow_assume" {
 }
 
 resource "aws_iam_role_policy" "flow" {
-  name = "${var.name}-flowlogs"
-  role = aws_iam_role.flow.id
+  count = var.enable_flow_logs ? 1 : 0
+  name  = "${var.name}-flowlogs"
+  role  = aws_iam_role.flow[0].id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -41,8 +45,9 @@ resource "aws_iam_role_policy" "flow" {
 }
 
 resource "aws_flow_log" "this" {
-  log_destination = aws_cloudwatch_log_group.flow.arn
-  iam_role_arn    = aws_iam_role.flow.arn
+  count           = var.enable_flow_logs ? 1 : 0
+  log_destination = aws_cloudwatch_log_group.flow[0].arn
+  iam_role_arn    = aws_iam_role.flow[0].arn
   traffic_type    = "ALL"
   vpc_id          = aws_vpc.this.id
   destination_options {
--- stacks/local/main.tf
+++ stacks/local/main.tf
@@ -2,8 +2,9 @@ module "vpc" {
   source                = "../../modules/vpc"
   name                  = "${var.name_prefix}-vpc"
   cidr                  = "10.30.0.0/16"
-  nat_gateway_count     = 1
+  nat_gateway_count     = 0
   create_public_subnets = true
+  enable_flow_logs      = false
   tags = {
     Name        = "${var.name_prefix}-vpc"
     Environment = "local"
